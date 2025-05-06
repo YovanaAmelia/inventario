17b9905ec9010d45ba1c084feeb9c49378a15329
amanda <amandaricra21@gmail.com>
2025-05-06 12:55:57 -0500

    lll

--- src/config/config.php
+++ src/config/config.php
@@ -8,7 +8,7 @@ const BD_PASSWORD = 'root';
 const BD_CHARSET = 'utf8';
 
 //ingresar link de proyecto
-const BASE_URL = 'http://localhost:8888/inventario/';
-const BASE_URL_SERVER = 'http://localhost:8888/inventario/';
+const BASE_URL = 'http://localhost/inventario/';
+const BASE_URL_SERVER = 'http://localhost/inventario/';
 
 ?>

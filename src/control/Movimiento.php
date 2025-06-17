0284196246facd36c976617260973c4c9403aa65
amanda <amandaricra21@gmail.com>
2025-06-17 16:36:58 -0500

    17 tarde

--- src/control/Movimiento.php
+++ src/control/Movimiento.php
@@ -208,9 +208,16 @@ if ($tipo == "datos_registro") {
     }
     echo json_encode($arr_Respuesta);
 }
+// estructurar un objeto con sus atributos n formato json solo atributos simples kkkkkkkkkkkkk
 if ($tipo == "buscar_movimiento_id") {
-  
+    $arr_Respuesta = array('status' => false, 'msg' => 'Error_Sesion');
     if ($objSesion->verificar_sesion_si_activa($id_sesion, $token)) {
-
-}
+       $id_movimiento =$_REQUEST['data'];
+       $arrMovimiento = $objMovimiento->buscarMovimientoById($id_movimiento);
+       $arrAmbOrigen = $objAmbiente->buscarAmbienteById($arrMovimiento->id_ambiente_origen);
+       $arrAmbDestino = $objAmbiente->buscarAmbienteById($arrMovimiento->id_ambiente_destino);
+       $arrUsuario = $objUsuario->buscarUsuarioById($arrMovimiento->id_usuario_registro);
+       $arrAmbIes = $objInstitucion->buscarInstitucionById($arrMovimiento->id_ies);
+    }
+echo json_encode($arrMovimiento);
 }
